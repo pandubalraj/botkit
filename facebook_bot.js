f8a56752316acb1f118947e9a37ae7346026fb13
pandubalraj <pandubalraj@gmail.com>
2017-01-24 09:07:18 +0530

    Update facebook_bot.js

--- facebook_bot.js
+++ facebook_bot.js
@@ -111,11 +111,11 @@ var controller = Botkit.facebookbot({
 var bot = controller.spawn({
 });
 
-controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
+controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
         if(ops.lt) {
-            var tunnel = localtunnel(process.env.port || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+            var tunnel = localtunnel(process.env.PORT || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
                 if (err) {
                     console.log(err);
                     process.exit();
@@ -397,11 +397,11 @@ controller.on('message_received', function(bot, message) {
 
 function formatUptime(uptime) {
     var unit = 'second';
-    if (uptime > 60) {
+    if (uptime > 90) {
         uptime = uptime / 60;
         unit = 'minute';
     }
-    if (uptime > 60) {
+    if (uptime > 90) {
         uptime = uptime / 60;
         unit = 'hour';
     }
